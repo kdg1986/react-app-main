99310177fe210671c4422fe6a37fafc31d307242
unknown <kdg1986>
2021-09-28 17:32:06 +0900

    샘플링

--- src/pages/system/Program.js
+++ src/pages/system/Program.js
@@ -1,4 +1,4 @@
-import { Row, Col,Select,Card,Divider,Button,DatePicker,Table,Input,Typography,Radio,Checkbox   } from 'antd';
+import { Row, Col,Select,Card,Divider,Button,DatePicker,Table,Input,Typography,Radio,Checkbox,Spin } from 'antd';
 import { SearchOutlined, EnvironmentOutlined } from '@ant-design/icons';
 import { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
@@ -46,24 +46,27 @@ RadioBtn.defaultProps = {
     defaultvalue : ""
 }
 
+const getSystemList = async () => {
+    const res = await axios.get(`${ELASTIC_URL}/demo/commonCode/systemList`)
+    return res;
+}
+
 const SearchOptions = props => {
 
     const style = {padding: '8px 0' };
 
     const [state,setState] = useState({
         systemName  : props.systemName  || [], selSystemName : "",
-        programGbn  : props.programGbn  || [],
+        programGbn  : props.programGbn  || [
+            { value : "", text : "전체"},
+            { value : "M", text : "메인"},
+            { value : "S", text : "서브"}
+        ],
         taskGbn1    : props.taskGbn1    || [], selTaskGbn1 : "",
         taskGbn2    : props.taskGbn2    || [],
         disabled1   : props.disabled1,
         disabled2   : props.disabled2,
     });
-
-    
-    const getSystemList = async () => {
-        const res = await axios.get(`${ELASTIC_URL}/demo/commonCode/systemList`)
-        return res;
-    }
 
     useEffect(()=>{        
         getSystemList().then((res)=>{
@@ -71,18 +74,18 @@ const SearchOptions = props => {
                 ...state
                 ,systemName : [ { value : "", text : "전체"}, ...JSON.parse(res.data._source.systemList) ]
             });
-        });        
-        //setSelect2({ data : _selData, selValue : "" });
+        });
+        return () => console.log('cleanup SearchOptions');        
     },[])
 
     const onChangeSystemName = async (value) => {
         if(value){
-            const res = await axios.get(`${ELASTIC_URL}/demo/commonCode/${value}`);        
+            const res = await axios.get(`${ELASTIC_URL}/demo/commonCode/${value}`);
             setState({ 
                 ...state,
                 taskGbn1 : [ { value : "", text : "전체"}, ...JSON.parse(res.data._source.taskList) ],
                 disabled1 : false,   
-                selSystemName : value             
+                selSystemName : value,                
             });
         }else{
             setState({ 
@@ -91,7 +94,7 @@ const SearchOptions = props => {
                 disabled1 : true,
                 taskGbn2 : [],
                 disabled2 : true,
-                selSystemName : value
+                selSystemName : value,                       
             });
         }
     }
@@ -99,12 +102,9 @@ const SearchOptions = props => {
     const onChangeTaskGbn1 = async (value) => {
         if(value){
             setState({ ...state, disabled2 : false, selTaskGbn1 : value });
-        }else{
+        }else{            
             setState({ 
                 ...state,
-                taskGbn1 : [],
-                disabled1 : true,
-                taskGbn2 : [],
                 disabled2 : true,
                 selTaskGbn1 : value
             });
@@ -121,23 +121,22 @@ const SearchOptions = props => {
                         defaultValue=""
                         style={{ width: 250 }} 
                         datasource={state.systemName} 
-                        onChange={onChangeSystemName} 
+                        onChange={onChangeSystemName}                         
                         />
                     </Col>
                     <Col span={2}>업무구분</Col>
                     <Col span={6}>
-                        <ComboBox                                 
-                        defaultValue=""
-                        style={{ width: 110 }} 
-                        datasource={state.taskGbn1} 
+                        <ComboBox
+                        style={{ width: 110 }}
+                        datasource={state.taskGbn1}
                         disabled={state.disabled1}
                         onChange={onChangeTaskGbn1}
                         />
                         <ComboBox 
-                        defaultValue="" 
-                        style={{ width: 110 }} 
-                        datasource={state.taskGbn2} 
-                        disabled={state.disabled2} 
+                        defaultValue=""
+                        style={{ width: 110 }}
+                        datasource={state.taskGbn2}
+                        disabled={state.disabled2}
                         />
                         <Input style={{ width: 150 }} defaultValue="" />
                     </Col>
@@ -147,11 +146,15 @@ const SearchOptions = props => {
                 <Row>
                     <Col span={2}>프로그램 구분</Col>
                     <Col span={6}>
-                        <ComboBox key="4" defaultValue="" style={{ width: 250 }} datasource={state.programGbn} />
+                        <ComboBox key="4" 
+                        defaultValue="" 
+                        style={{ width: 250 }} 
+                        datasource={state.programGbn} 
+                        />
                     </Col>
                     <Col span={2}>수정일자</Col>
                     <Col span={6}>
-                        <DatePicker /> ~ <DatePicker />
+                        <DatePicker onChange={(date,str)=> setState({ ...state, startDt : str })  } /> ~ <DatePicker onChange={(date,str)=> setState({ ...state, endDt : str })  } />
                     </Col>
                 </Row>
             </div>
@@ -164,7 +167,7 @@ const SearchOptions = props => {
 
 }
 
-const DataList = props => {
+const DataList = props => {    
     return(
         <Table                     
         onRow={(record, rowIndex) => {
@@ -180,36 +183,97 @@ const DataList = props => {
             //onChange : (val) => console.log(val)                        
         }}
         columns={[
-            { title: 'No', dataIndex: 'no', },
+            { title: 'No', dataIndex: 'C'},
             { title: '시스템명', dataIndex: 'systemName', },
             { title: '업무구분', dataIndex: 'task', },
-            { title: '프로그램구분', dataIndex: 'programGbn', render : text => text === "M" ? "메인" : "서브" },
-            { title: '프로그램 NO', dataIndex: 'programNo', },
+            { title: '프로그램구분', dataIndex: 'programGbn', render : text => text === "M" ? "메인" : "서브"},
+            { title: '프로그램 NO', dataIndex: 'programNo', sorter : (a,b) => a.programNo > b.programNo  },
             { title: '프로그램명', dataIndex: 'E', },
             { title: '프로그램 URL', dataIndex: 'programURL', },
             { title: '프로그램 파일명', dataIndex: 'programFileName', },
             { title: '사용', dataIndex: 'useYN', render : (text, record, index) => <Checkbox key={uuidv4()} checked={text === "Y" ? true : false}></Checkbox>    },
-            { title: '수정일자', dataIndex: 'modiDate', },
+            { title: '수정일자', dataIndex: 'modiDate', sorter : (a,b) => a.modiDate < b.modiDate ? -1 : a.modiDate > b.modiDate ? 1 : 0},
         ]}
         dataSource={props.data} 
         />
     )
 }
 
-export default () => {    
-    const onChange = e => setValue(e.target.value);
+export default () => {        
     const { TextArea } = Input;
     const { Title } = Typography;    
     const style = {padding: '8px 0' };        
     const [ data, setData ] = useState([]);
     const [ detail, setDetail ] = useState();
-    const dataRowOnClick = (record,rowIndex) => setDetail({ ...record });
     const doSearch = async (state) => {
         const res = await axios.get(`${ELASTIC_URL}/program/list/${state.selTaskGbn1}`);
-        setData(JSON.parse( res.data._source.programList ).map( item => { item.key = uuidv4(); return item }));
+        setData(JSON.parse( res.data._source.programList ).map( (item,idx) => { item.key = idx; return item }));
     }
-    const handleChange = e => setDetail({ ...detail, [e.target.name]: e.target.value });
-    
+    const handleChange = e => {        
+        setDetail({ ...detail, [e.target.name]: e.target.value });
+    }
+    const doSave = () => {        
+        const {
+            C,E,I,            
+            programFileName,
+            programGbn,
+            programNo,
+            programURL,
+            systemName,
+            dtlTaskVal : task,
+            useYN,
+            modiDate,
+            key
+        } = detail;
+        const _data = [ ...data ];
+        
+        _data[detail.key] = {
+            C,
+            E,
+            I,   
+            key,
+            programFileName,
+            programGbn,
+            programNo,
+            programURL,
+            systemName,
+            task,
+            useYN,
+            modiDate,
+        }
+        setData(_data);
+    }
+
+    const dataRowOnClick = (record,rowIndex) => {        
+        setDetail({  spin : true });
+        const res = axios.get(`${ELASTIC_URL}/demo/commonCode/${record.systemName}`);
+        res.then(( res )=>{
+            setDetail({ 
+            ...detail,
+            //orgRecord : {...record},
+            ...record,
+            rowIndex : rowIndex,
+            dtlTaskList : JSON.parse(res.data._source.taskList),
+            dtlTaskVal : record.task 
+            });
+        })
+    }
+
+    useEffect(()=>{        
+        console.log(1)
+        getSystemList().then((res)=>{
+            setDetail({ 
+                ...detail
+                ,dtlSystemName : [ ...JSON.parse(res.data._source.systemList) ]
+                ,dtlProgramGbn : [                    
+                    { value : "M", text : "메인"},
+                    { value : "S", text : "서브"}
+                ]
+            });
+        });        
+        return () => console.log('cleanup');
+    },[])
+
     return(
         <>
             <Card>
@@ -230,8 +294,7 @@ export default () => {
                         <Row justify="end">                            
                             <div style={{padding: '0 3px' }}> <Button icon={<SearchOutlined />}>신규</Button> </div>
                             <div style={{padding: '0 3px' }}> <Button icon={<SearchOutlined />}>삭제</Button> </div>
-                            <div style={{padding: '0 3px' }}> <Button icon={<SearchOutlined />}>취소</Button> </div>
-                            <div style={{padding: '0 3px' }}> <Button icon={<SearchOutlined />}>저장</Button> </div>
+                            <div style={{padding: '0 3px' }}> <Button icon={<SearchOutlined />}>취소</Button> </div>                            
                             <div style={{padding: '0 3px' }}> <Button icon={<SearchOutlined />}>엑셀</Button> </div>                            
                         </Row>
                     </Col>
@@ -242,16 +305,41 @@ export default () => {
             </div>
 
             <div style={style}>
-                <Title level={4}><EnvironmentOutlined /> 프로그램 상세 정보</Title>
-                <Card>
+                <Row>
+                    <Col span={12}>
+                        <Row>
+                            <Title level={4}><EnvironmentOutlined /> 프로그램 상세 정보</Title>                   
+                        </Row>
+                    </Col>
+                    <Col span={12}>
+                        <Row justify="end">                            
+                            <div style={{padding: '0 3px' }}> <Button icon={<SearchOutlined />} onClick={doSave} >저장</Button> </div>
+                        </Row>
+                    </Col>
+                </Row>
+                <Card>                
                 <Row>
                     <Col span={2}>시스템명</Col>
                     <Col span={6}>
-                        {/*<ComboBox key="4" defaultValue="" style={{ width: 250 }} datasource={} />*/}
+                        <ComboBox                         
+                        key="4"
+                        defaultValue=""
+                        style={{ width: 250 }}
+                        datasource={detail?.dtlSystemName}
+                        value={detail?.systemName}
+                        onChange={(val) => setDetail({ ...detail,systemName : val })}
+                         />
                     </Col>
                     <Col span={2}>업무구분</Col>
-                    <Col span={6}>
-                        
+                    <Col span={6}>                    
+                        <ComboBox                         
+                        key="5"
+                        defaultValue=""
+                        style={{ width: 250 }}
+                        datasource={detail?.dtlTaskList}
+                        value={detail?.dtlTaskVal}
+                        onChange={(val) => setDetail({ ...detail,dtlTaskVal : val })}
+                         />
                     </Col>
                 </Row>
             </Card>
@@ -262,7 +350,16 @@ export default () => {
                         <Input name="programNo" style={{ width: 200 }} value={detail?.programNo} onChange={handleChange} />
                     </Col>
                     <Col span={2}>프로그램 구분</Col>
-                    <Col span={6}></Col>
+                    <Col span={6}>
+                        <ComboBox 
+                        key="4"
+                        defaultValue=""
+                        style={{ width: 250 }}
+                        datasource={detail?.dtlProgramGbn}
+                        value={detail?.programGbn}
+                        disabled={true}
+                         />
+                    </Col>
                 </Row>
             </Card>
             <Card>
